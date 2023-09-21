f359be529b44a726356d2978558cba99e307ef75
Nguyen Hiep Loi <loi.nguyen@HBG.COM>
2023-09-21 17:36:14 +0700

    done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      spacing:{
+        '35':'35px',
+      }
+    },
+  },
+  plugins: [],
+}
